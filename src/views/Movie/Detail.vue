746e3f1ca5f9ef6e40407933e1457edd35c98c22
baron <1942992735@qq.com>
2019-08-06 14:57:12 +0800

    bug clear

--- src/views/Movie/Detail.vue
+++ src/views/Movie/Detail.vue
@@ -6,7 +6,8 @@
     <Loading v-if="isLoading" />
     <div v-else id="content" class="contentDetail">
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <!-- <div class="detail_list_bg" :style="{'background-image':'url('+detailMovie.img | setWH('148.208')+')'}"></div> -->
+        <div class="detail_list_bg" :style="{'background-image':'url('+detailMovie.img.replace(/w\.h/,'148.208')+')'}"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
@@ -46,7 +47,7 @@ export default {
   data() {
     return {
       detailMovie: {},
-      isLoading : true
+      isLoading: true
     };
   },
   components: {
